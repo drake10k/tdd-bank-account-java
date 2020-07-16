f9a6461e5d1987066b7277264f6c00ae9990d892
drake10k <28574547+drake10k@users.noreply.github.com>
2020-07-16 17:01:27 +0300

    made balance a double

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -2,9 +2,9 @@ package org.xpdojo.bank;
 
 public class Account {
 
-    private int balance = 0;
+    private double balance = 0;
 
-    public int balance() {
+    public double balance() {
         return balance;
     }
 
